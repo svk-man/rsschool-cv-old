69e65203cf68bcd201209c730f016e9917510c98
Sergey Kravchenko <kravchenko.sgv@gmail.com>
2021-09-21 12:15:21 +0300

    refactor: update some information about me

--- script.js
+++ script.js
@@ -8,3 +8,12 @@ function toggle() {
     hamburgerMenu.classList.add('open');
   }
 }
+
+var str = 'Я продолжаю разработку. Контакты: discord: @svk-man';
+var style = ['padding: 1rem;',
+  'background: linear-gradient( gold, orangered);',
+  'text-shadow: 0 2px orangered;',
+  'font: 1.3rem/3 Georgia;',
+  'color: white;'].join('');
+
+console.log ( '%c%s', style, str );
